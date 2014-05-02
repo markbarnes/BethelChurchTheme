c9fc1891911faad3fa89380bd3d7116dd094173e
Mark Barnes <mark@sharonandmark.net>
2014-05-02 18:19:53 +0100

    Remove mobile support for now.

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ define( 'CHILD_THEME_VERSION', '2.0.1' );
 * Add theme support
 */
 add_theme_support( 'html5' ); //* Add HTML5 markup structure
-add_theme_support( 'genesis-responsive-viewport' ); //* Add viewport meta tag for mobile browsers
+//add_theme_support( 'genesis-responsive-viewport' ); //* Add viewport meta tag for mobile browsers
 add_theme_support( 'custom-background', bethel_get_default_background_args()); //* Add support for custom background
 add_theme_support( 'genesis-footer-widgets', 3 ); //* Add support for 3-column footer widgets
 
