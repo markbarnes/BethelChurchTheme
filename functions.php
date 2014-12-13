c52cfb635ff8de1c4b4060ab1e4beedf773e6ecd
Mark Barnes <mark@sharonandmark.net>
2014-12-13 18:44:03 +0000

    Major update to 0.2

--- functions.php
+++ functions.php
@@ -4,30 +4,33 @@ require_once( get_template_directory() . '/lib/init.php' );
 require ('inc/theme_functions.php');
 
 //* Child theme (do not remove)
-define( 'CHILD_THEME_NAME', 'Custom theme for Bethel, Clydach' );
-define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
+define( 'CHILD_THEME_NAME', 'Bethel, Clydach' );
+define( 'CHILD_THEME_URL', 'http://www.markbarnes.net/' );
 define( 'CHILD_THEME_VERSION', '0.1' );
 
 /**
 * Add theme support
 */
-add_theme_support( 'html5' ); //* Add HTML5 markup structure
+add_theme_support( 'html5', array('comment-list', 'comment-form', 'search-form', 'gallery', 'caption'));
 add_theme_support( 'custom-background', bethel_get_default_background_args()); //* Add support for custom background
 add_theme_support( 'genesis-footer-widgets', 3 ); //* Add support for 3-column footer widgets
+
 
 /**
 * Remove default actions
 */
-remove_action( 'wp_head', 'genesis_load_favicon' );
-remove_action( 'genesis_meta', 'genesis_responsive_viewport' );
+remove_action( 'wp_head', 'genesis_load_favicon' ); // Don't use the default favicon
+remove_action( 'genesis_meta', 'genesis_responsive_viewport' ); // Turn off responsive stylying
 remove_action( 'genesis_site_title', 'genesis_seo_site_title' ); // Remove the site title
 remove_action( 'genesis_site_description', 'genesis_seo_site_description' ); // Remove the subtitle
 remove_action( 'genesis_after_header', 'genesis_do_subnav' ); // Remove the subnav from below the header
-remove_action( 'genesis_footer', 'genesis_do_footer' );
+remove_action( 'genesis_footer', 'genesis_do_footer' ); // Remove the default footer
+
 /**
 * Add actions
 */
 add_action( 'wp_enqueue_scripts', 'bethel_enqueue_fonts' ); // Enqueue fonts
+add_action( 'wp_enqueue_scripts', 'bethel_add_frontend_javascript' );
 add_action ('admin_enqueue_scripts', 'bethel_admin_enqueue_fonts');
 add_action ('login_enqueue_scripts', 'bethel_add_login_logo'); // Add CSS for login logo
 add_action( 'genesis_meta', 'bethel_add_favicons'); // Add favicons
@@ -43,19 +46,31 @@ add_action ('genesis_footer', 'bethel_do_footer_bottom');
 add_action ('genesis_meta', 'bethel_add_image_to_pages', 11);
 add_action ('admin_head', 'bethel_add_favicons');
 add_action ('admin_head', 'bethel_add_admin_css');
+add_action ('template_redirect', 'bethel_custom_template');
 
 /**
 * Add filters
 */
 add_filter('genesis_footer_creds_text', 'bethel_footer'); //* Change the footer text
-add_filter ('wp_nav_menu_args', 'bethel_restrict_depth_of_primary_menu');
-add_filter ('image_size_names_choose', 'bethel_choose_image_sizes');
+add_filter ('wp_nav_menu_args', 'bethel_restrict_depth_of_primary_menu'); // Restricts the menu menu to two levels
+add_filter ('image_size_names_choose', 'bethel_choose_image_sizes'); 
+add_filter ('genesis_edit_post_link', create_function ('$args', 'return false;'));
+add_filter ('gallery_style', 'bethel_add_filter_to_gallery_images');
 
 /**
 * Add image sizes
 */
-add_image_size ('bethel_column_width', 360);
-add_image_size ('bethel_full_width', 660);
-add_image_size ('bethel_supersize', 740);
+add_image_size ('bethel_narrow_column', 360);
+add_image_size ('bethel_column_width', 400);
+add_image_size ('bethel_full_width', 720);
+add_image_size ('bethel_supersize', 800);
 
+/**
+* Register widget areas
+*/
 bethel_register_widget_areas();
+
+/**
+* Add shortcodes
+*/
+add_shortcode ('pullquote', 'bethel_pullquote');
--- inc/theme_functions.php
+++ inc/theme_functions.php
@@ -2,17 +2,47 @@
 /**
 * Enqueue fonts
 * 
+* Called by the wp_enqueue_scripts action
 */
 function bethel_enqueue_fonts() {
 	wp_enqueue_style ('bethel-font-lato', get_stylesheet_directory_uri().'/fonts/lato.css', array(), CHILD_THEME_VERSION);
-	//wp_enqueue_style ('bethel-font-bebas', get_stylesheet_directory_uri().'/fonts/bebas.css', array(), CHILD_THEME_VERSION);
 	wp_enqueue_style ('bethel-font-icons', get_stylesheet_directory_uri()."/fonts/bethel-icons.css", array(), CHILD_THEME_VERSION);
 }
 
+/**
+* Enqueue admin fonts
+* 
+* Called by the admin_enqueue_scripts action
+*/
 function bethel_admin_enqueue_fonts() {
 	wp_enqueue_style ('bethel-font-icons', get_stylesheet_directory_uri()."/fonts/bethel-icons.css", array(), CHILD_THEME_VERSION);
 }
 
+/**
+* Enqueue javascript
+* 
+* Called by the wp_enqueue_scripts action
+*/
+function bethel_add_frontend_javascript() {
+    global $post;
+    if (is_front_page()) {
+        wp_enqueue_script('bethel-frontend', get_stylesheet_directory_uri().'/js/frontend.js', array('jquery'), '0.1', true);
+        wp_localize_script('bethel-frontend', 'bethel', array('siteurl'=>site_url()));
+    }
+    if ((strpos ($post->post_content, '[gallery ') !== FALSE)) {
+        wp_enqueue_script('jquery');
+        add_action ('wp_print_footer_scripts', 'bethel_add_gallery_js_to_footer');
+        add_filter ('shortcode_atts_gallery', 'bethel_filter_gallery_atts', 10, 3);
+        add_filter ('body_class', 'bethel_add_gallery_to_body_class');
+    }
+}
+
+/**
+* Returns the default background settings
+* 
+* Called due to custom-background in add_theme_support
+* 
+*/
 function bethel_get_default_background_args() {
 	return array ('default-color' => '#f5f5f5',
 				  'default-image' => get_stylesheet_directory_uri().'/images/background.jpg',
@@ -20,14 +50,30 @@ function bethel_get_default_background_args() {
 	             );
 }
 
+/**
+* Adds the filter to add subtitles to the main menu
+* 
+* Called by the genesis_after_header action (early priority)
+*/
 function bethel_filter_menu_items() {
-	add_filter ('walker_nav_menu_start_el', 'bethel_add_menu_subtitles'); // Adds subtitles to menus
+	add_filter ('walker_nav_menu_start_el', 'bethel_add_menu_subtitles');
 }
 
+/**
+* Removes filter that added subtitles to menus
+* 
+* Called by the genesis_after_header action (late priority)
+*/
 function bethel_stop_filtering_menu_items() {
-	remove_filter ('walker_nav_menu_start_el', 'bethel_add_menu_subtitles'); // Removes filter that added subtitles to menus
+	remove_filter ('walker_nav_menu_start_el', 'bethel_add_menu_subtitles');
 }
 
+/**
+* Displays subtitles in the main menu
+* 
+* @param mixed $string
+* @return string
+*/
 function bethel_add_menu_subtitles ($string) {
 	if (substr($string, 3, 7) == 'title="') {
 		$title = substr($string, 10, strpos($string, '"', 11)-10);
@@ -36,14 +82,32 @@ function bethel_add_menu_subtitles ($string) {
 	return $string;
 }
 
-function bethel_footer( $creds ) {
+/**
+* Displays the footer
+* 
+* Filters genesis_footer_creds_text
+* 
+* @param mixed $creds
+* @return mixed
+*/
+function bethel_footer ($creds) {
 	return '<span class="footer-line-1"><span class="bethel-tree-icon"></span> &nbsp;<strong>Bethel Evangelical Church, Heol-y-nant, Clydach</strong></span><br/><span class="footer-line-2">Tel: 01792 828095 &nbsp;&nbsp;&nbsp; Registered charity: 1142690</span>';
 }
 
+/**
+* Displays the logo in the header
+* 
+* Called by the genesis_site_title action
+*/
 function bethel_do_logo() {
 	echo "<a class=\"logo_header\" href=\"".home_url()."\"><img src=\"".get_stylesheet_directory_uri()."/images/logo.png\" width=\"600\" height=\"139\" title=\"Bethel Evangelical Church, Clydach\" alt=\"Bethel Evangelical Church, Clydach\"/></a>";
 }
 
+/**
+* Adds code to the head, to display favicons and shortcut icons
+* 
+* Called by the genesis_meta action
+*/
 function bethel_add_favicons() {
 	echo "<link rel=\"shortcut icon\" href=\"".get_stylesheet_directory_uri()."/images/favicons/favicon.ico\">\r\n";
 	echo "<link rel=\"icon\" sizes=\"16x16 32x32 64x64\" href=\"".get_stylesheet_directory_uri()."/images/favicons/favicon.ico\">\r\n";
@@ -62,10 +126,21 @@ function bethel_add_favicons() {
 	echo "<meta name=\"msapplication-config\" content=\"".get_stylesheet_directory_uri()."/images/favicons/browserconfig.xml\">\r\n";
 }
 
+/**
+* Resizes the viewport for mobile devices
+* 
+* Called by the genesis_meta action
+*/
 function bethel_add_viewport() {
 	echo "<meta name=\"viewport\" content=\"width=1200\">\r\n";
 }
 
+/**
+* Adds the Bethel log to the log in page
+* 
+* Called by the login_enqueue_scripts action
+* 
+*/
 function bethel_add_login_logo() {
 	echo "<style type=\"text/css\">\r\n";
 	echo "\tbody.login div#login h1 a {\r\n";
@@ -78,6 +153,11 @@ function bethel_add_login_logo() {
 	echo "</style>";
 }
 
+/**
+* Adds custom CSS to the admin pages
+* 
+* Called by the admin_head action
+*/
 function bethel_add_admin_css() {
 	echo <<<EOT
 	<style type="text/css">
@@ -89,6 +169,11 @@ function bethel_add_admin_css() {
 EOT;
 }
 
+/**
+* Adds the submenu with sister pages to the current post, if appropriate
+* 
+* Called by the genesis_entry_content action
+*/
 function bethel_add_submenu_to_post() {
 	global $post;
 	$menu_items = get_menu_items_for_current_page();
@@ -103,6 +188,11 @@ function bethel_add_submenu_to_post() {
 	}
 }
 
+/**
+* Returns an array of menu items for the current page
+* 
+* @return array
+*/
 function get_menu_items_for_current_page () {
 	global $post;
 	if (!$post->ID) {
@@ -140,13 +230,13 @@ function bethel_add_image_to_pages() {
 	if ($post) {
 		$image_id = get_post_thumbnail_id ($post->ID);
 		if ($image_id) {
-			if ($post->post_type != 'bethel_stories') {
+			if ($post->post_type != 'bethel_stories' && $post->page_template != 'stories.php') {
 				$image = wp_get_attachment_image_src($image_id, 'bethel_supersize');
 				if ($image) {
-					if ($image[1] < 740) { //width
-						$image[2] = round($image [2] / $image[1] * 740);
+					if ($image[1] < 800) { //width
+						$image[2] = round($image [2] / $image[1] * 800);
 					}
-					$image[2] = ($image[2] > 370) ? 370 : $image[2]; //height
+					$image[2] = ($image[2] > 400) ? 400 : $image[2]; //height
 					echo "<style type=\"text/css\">.entry-header { height: {$image[2]}px; background-image: url('{$image[0]}')}</style>";
 					return;
 				}
@@ -162,7 +252,7 @@ function bethel_add_image_to_pages() {
 */
 function bethel_add_image_to_stories() {
 	global $post;
-	if ($post && $post->post_type=='bethel_stories') {
+	if ($post && ($post->post_type=='bethel_stories' || $post->page_template == 'stories.php')) {
 		echo "<div class=\"bethel-featured-image-wrapper\">";
 		the_post_thumbnail('bethel_column_width');
 		$caption = apply_filters ('bethel_featured_image_caption', '');
@@ -174,6 +264,14 @@ function bethel_add_image_to_stories() {
 	}
 }
 
+/**
+* Restricts the dept of the primary menu to three levels (deeper levels are shown in the post itself)
+* 
+* Filters wp_nav_menu_args
+* 
+* @param array $args
+* @return array
+*/
 function bethel_restrict_depth_of_primary_menu ($args) {
 	$a=1;
 	if ($args['theme_location'] == 'primary') {
@@ -182,24 +280,134 @@ function bethel_restrict_depth_of_primary_menu ($args) {
 	return $args;
 }
 
+/**
+* Adds additional image sizes when editing a post
+* 
+* Filters image_size_names_choose
+* 
+* @param array $sizes
+* @return array
+*/
 function bethel_choose_image_sizes ($sizes) {
-	$bethel_sizes = array 	('bethel_column_width' => 'Half-width',
+	$bethel_sizes = array 	('bethel_narrow_column' => 'Narrow',
+                             'bethel_column_width' => 'Half-width',
 							 'bethel_full_width' => 'Full-width',
 							 'bethel_supersize' => 'Supersize',
 							);
 	return array_merge ($sizes, $bethel_sizes);
 }
 
+/**
+* Registers the themes widget areas
+*/
 function bethel_register_widget_areas() {
 	unregister_sidebar( 'header-right' ); // Remove the right header widget area
 	genesis_register_sidebar (array ('id' => 'header-right-top', 'name' => 'Header Right Top', 'description' => 'The upper widget to the right of the header image.'));	
 	genesis_register_sidebar (array ('id' => 'footer-bottom', 'name' => 'Footer Bottom', 'description' => 'Full-width widget at the very bottom of the page.'));	
 }
 
+/**
+* Outputs the top-right widget area
+*/
 function bethel_do_header_right_top() {
 	genesis_widget_area ('header-right-top', array ('before' => '<div id="bethel-header-right-top">', 'after' => '</div>'));
 }
 
+/**
+* Outputs the widget below the footer
+*/
 function bethel_do_footer_bottom() {
 	genesis_widget_area ('footer-bottom', array ('before' => '<div id="bethel-header-right-bottom">', 'after' => '</div>'));
+}
+
+/**
+* Add the page template to the global $post variable
+* 
+* Called by the template_redirect action
+*/
+function bethel_custom_template() {
+    if (is_page()) {
+        global $post;
+        $post->page_template = get_post_meta( $post->ID, '_wp_page_template', true );
+    }
+}
+
+/**
+* Handles the pullquote shortcode
+*/
+function bethel_pullquote ($atts, $content = NULL) {
+    if (isset($atts['align']) && $atts['align']=='right') {
+        $att_string = ' class="align-right"';
+    } else {
+        $att_string = '';
+    }
+    return "<aside{$att_string}><span class=\"left-quote\">&ldquo;</span>{$content}<span class=\"right-quote\">&rdquo;</span></aside>";
+}
+
+/**
+* Adds the 'gallery' class to the body tag
+* 
+* Filters body_class
+* 
+* @param array $class
+* @return array
+*/
+function bethel_add_gallery_to_body_class ($classes) {
+    $classes [] = 'bethel-gallery';
+    return $classes;
+}
+
+/**
+* Adds javascript to the footer on gallery pages
+* 
+* Called on the wp_print_footer_scripts action
+*/
+function bethel_add_gallery_js_to_footer() {
+    echo "<script type=\"text/javascript\">\r\n";
+    include ('gallery.js.php');
+    echo "</script>\r\n";
+}
+
+/**
+* Adds a filter to gallery images
+* 
+* Filters gallery_style (or, more accurately, actions during the filter)
+* 
+* @param string $style
+* @return string
+*/
+function bethel_add_filter_to_gallery_images ($style) {
+    add_filter ('wp_get_attachment_image_attributes', 'bethel_filter_image_attributes_for_gallery', 10, 2);
+    add_filter ('genesis_edit_post_link', 'bethel_remove_filter_from_gallery_images'); // This filter runs at the end of a post
+    return $style;
+}
+
+/**
+* Filters the image attributes in galleries
+* 
+* @param mixed $attr
+* @param mixed $attachment
+*/
+function bethel_filter_image_attributes_for_gallery ($attr, $attachment) {
+    $attr ['id'] = "gallery-image-id-{$attachment->ID}";
+    $attr ['class'] .= ' gallery-thumbnail';
+    return $attr;
+}
+
+function bethel_remove_filter_from_gallery_images ($edit) {
+    remove_filter ('wp_get_attachment_image_attributes', 'bethel_filter_image_attributes_for_gallery', 10, 2);
+    return $edit;
+}
+
+function bethel_filter_gallery_atts ($out, $pairs, $atts) {
+    $ids = $out ['include'];
+    if ($ids) {
+        global $bethel_gallery_urls;
+        $ids = explode (',', $ids);
+        foreach ($ids as $id) {
+            $urls[$id] = wp_get_attachment_url($id, 'bethel_supersize');
+        }
+        $bethel_gallery_urls = json_encode ($urls);
+    }
+    return $out;
 }
